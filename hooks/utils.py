394b97466323cee3ac2d44f5bf99cf7023583b5f
robcxyz <rob@robot.localdomain>
2019-06-25 13:22:49 -0700

    added aws util

---
+++ hooks/utils.py
@@ -0,0 +1,27 @@
+import boto3
+import json
+
+
+def get_availability_zones():
+    client = boto3.client('ec2')
+    regions =[]
+    zones = {}
+    for region in client.describe_regions()["Regions"]:
+        regions.append(region['RegionName'])
+    regions = sorted(regions)
+    for region in regions:
+        client = boto3.client('ec2', region_name = region)
+        zones[region] = []
+        for zone in client.describe_availability_zones()['AvailabilityZones']:
+            if zone['State'] == 'available':
+                zones[region].append(zone['ZoneName'])
+
+    return zones
+
+def print_availability_zones():
+    with open('aws_availability_zones.json', 'w') as f:
+        json.dump(get_availability_zones(), f)
+
+
+if __name__ == '__main__':
+    print_availability_zones()
